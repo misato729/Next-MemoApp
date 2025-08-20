753953540a91a616070e42bb18ea44a5d1a5c3b1
misato729 <valor729@icloud.com>
2025-08-21 07:35:13 +0900

    謎のコメントアウト削除

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -13,10 +13,8 @@ export default async function EditPage({
   return (
     <main className="grid grid-cols-1 lg:grid-cols-[18rem_1fr] gap-6">
       {/* 左：一覧 */}
-      {/* @ts-expect-error Client Component */}
       <MemoList />
       {/* 右：編集 */}
-      {/* @ts-expect-error Client Component */}
       <MemoView id={id} mode="edit" />
     </main>
   );
